1401b19c26e04b310e634dd6ebd09a1a17c17dfd
hamed ghafori <hamedghafori2022@gmail.com>
2023-05-29 20:24:39 +0330

    adding file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
 function App() {
   return (
     <BrowserRouter>
     <Navbar/>
+    <Home/>
     </BrowserRouter>
   )
 }
