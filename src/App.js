aba6b4e39a32a8f8fb4c19c3c736ad545637aa04
hamed ghafori <hamedghafori2022@gmail.com>
2023-05-29 20:09:31 +0330

    adding

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Navbar/>
+    </BrowserRouter>
   )
 }
 
