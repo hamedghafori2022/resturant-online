69a44c68cd5369fc47092b7b043a9b8c0cd0c0f4
hamed ghafori <hamedghafori2022@gmail.com>
2023-05-29 22:06:11 +0330

    adding footer

--- src/components/SharedLayout.js
+++ src/components/SharedLayout.js
@@ -3,11 +3,13 @@ import {  Outlet } from "react-router-dom";
 
 import "../styles/Home.css";
 import Navbar from "./Navbar";
+import Footer from './Footer'
 function Home() {
   return (
     <>
     <Navbar/>
     <Outlet/>
+    <Footer/>
     </>
   );
 }
